3d8cf42873abae91dc721d04a5f10ac3c7a991bc
Kevin Shah <kevinshah@KevinsSchoolMBP.fios-router.home>
2020-11-12 20:16:41 -0500

    centered about page

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function About() {
     return (
-        <div>
+        <div style={{textAlign: 'center'}}>
             <h1>About Page</h1>
             <p>This is a Todo List app made using React JS</p>
         </div>
